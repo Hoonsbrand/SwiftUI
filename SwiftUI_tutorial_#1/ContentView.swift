0ffdef0a568f8b9bda2e979c5178e14a02674d83
Hoonsbrand <63503972+Hoonsbrand@users.noreply.github.com>
2023-03-09 16:31:47 +0900

    정대리 SwiftUI 1강

--- SwiftUI_tutorial_#1/ContentView.swift
+++ SwiftUI_tutorial_#1/ContentView.swift
@@ -9,16 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        HStack {
+            MyVStackView()
+            MyVStackView()
+            MyVStackView()
+        }   // HStack
+        .padding(.all, 10)
+        .background(Color.yellow)
     }
 }
 
+// 보여주는 용도
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
